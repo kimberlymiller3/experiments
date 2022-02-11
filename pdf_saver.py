04b8a955a64178d22148c64e61620e5d3a7f2e3b
fineanganofoj <fineanganofoj@moravian.edu>
2022-02-11 14:49:47 -0500

    remove print statement

--- pdf_saver.py
+++ pdf_saver.py
@@ -14,9 +14,9 @@ ATTACHMENT_URL = os.getenv('ATTACHMENT_URL')
 USERNAME = os.getenv('USERNAME')
 AUTH_KEY = os.getenv('AUTH_KEY')
 
-r = requests.get(ATTACHMENT_URL, auth=(USERNAME, AUTH_KEY))
+r = requests.get(ATTACHMENT_URL)
 file_name = r.headers['Content-Disposition'].split('"')[1]
 content = r.content
-print(file_name)
+
 with open(file_name, 'wb') as f:
     f.write(content)
