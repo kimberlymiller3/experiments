69aac2217f349645a37fba1be6caf4aea302296c
Kimberly Miller <millerk10@moravian.edu>
2022-02-16 00:17:56 -0500

    Updated flask server to handle posting along with downloading attachment

--- attachment_flask.py
+++ attachment_flask.py
@@ -8,23 +8,22 @@ app = Flask(__name__)
 
 ATTACHMENT_URL = "https://downloads.regulations.gov/EPA-HQ-OW-2021-0602-0130/attachment_1.pdf"
 
-def attachment_url():
-    r = requests.get(ATTACHMENT_URL)
-    file_name = r.headers['Content-Disposition'].split('"')[1]
-    content = r.content
-    return content, file_name
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'DOWNLOAD'])
 def upload_file():
-    render_template('index.html')
-    r = requests.get(ATTACHMENT_URL)
-    file_name = r.headers['Content-Disposition'].split('"')[1]
-    content = r.content
-    if file_name != '':
-        with open(file_name, 'wb') as f:
-            f.write(content)
+    if request.method == 'POST':
+        if request.form.get('Upload') == 'Upload File':
+            r = requests.get(ATTACHMENT_URL)
+        elif  request.form.get('Download') == 'Download File':
+            r = requests.get(ATTACHMENT_URL)
+            file_name = r.headers['Content-Disposition'].split('"')[1]
+            content = r.content
+            if file_name != '':
+                with open(file_name, 'wb') as f:
+                    f.write(content)
     return redirect(url_for('index'))
+
